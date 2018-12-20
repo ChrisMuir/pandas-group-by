ee9735056c8988781a907eea458c63b7fd11f937
ChrisMuir <chrismuirRVA@gmail.com>
2018-12-20 15:08:53 -0500

    add README as long_description

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@
 
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pandas_group_by',
       version='0.0.1',
       description='An implementation of pandas.groupby() that will keep and display NaN as a group',
+      long_description=readme(),
+      keywords='pandas groupby',
       url='http://github.com/ChrisMuir/pandas-group-by',
       author='Chris Muir',
       author_email='chrismuirrva@gmail.com',
