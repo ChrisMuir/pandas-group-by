53e60bace6c62df86a1b352eaebc38f2f0a891ce
ChrisMuir <chrismuirRVA@gmail.com>
2018-12-20 12:58:06 -0500

    check for invalid input col strings

--- pandas_group_by/__init__.py
+++ pandas_group_by/__init__.py
@@ -20,6 +20,11 @@ def group_by(df, cols, **kwargs):
         raise ValueError("arg 'cols' must be column headers to group by, as a single string or a list of strings")
     if isinstance(cols, str):
         cols = [cols]
+    if not all([n in df.columns for n in cols]):
+        missings = ", ".join([n for n in cols if n not in df.columns])
+        raise ValueError("All input 'cols' must appear as columns in 'df'\n"\
+                         "The following input col strings are not columns in df:\n"\
+                         "%s" % missings)
     
     # Perform grouping operation.
     res = df.groupby([df[col].replace(nan, "NaN") for col in cols], **kwargs)
